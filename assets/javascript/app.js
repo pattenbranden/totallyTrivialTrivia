7e914c29e695d2390f9fad31451a39d55c2e3354
pattenbranden <patten.branden@gmail.com>
2020-01-17 13:57:47 -0500

    initial project files

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// use custom font
+// greet player with instructions and start button on document ready 
+// hide instructions on game start 
+// inside a div put a timer feature that will run down to zero, 
+// when timer hits zero, input as if wrong answer 
+// congradulate player on correct answer // on timer offer next question 
+// do similar with wrong answer, show correct answer
+// after round is complete offer results and the option to resset the script 
