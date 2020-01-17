1a24eac4c91fca39c07bbfbb5794b2aedc14bd50
pattenbranden <patten.branden@gmail.com>
2020-01-17 15:48:34 -0500

    added questionbank object, restructured the answer list span, and added a currently unusued bg.jpg

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,3 +6,55 @@
 // congradulate player on correct answer // on timer offer next question 
 // do similar with wrong answer, show correct answer
 // after round is complete offer results and the option to resset the script 
+
+var qNum = 0
+var wrongAnswer
+var correctAnswer
+
+
+var qBank = {
+
+q0: {
+    text: "how long is a foot?",
+    wa: ["6 centimeters", "10 toes", "18 pounds"],
+    ca: "12 inches",
+    additionalInfo: "did you know that 12 inches is also not that big"
+    },
+
+q1: {
+    text: "how heavy is a pound of feathers?",
+    wa: ["3 stone", "12 kilograms", "14 miles"],
+    ca: "a pound",
+    additionalInfo: "did you know that a pound of steel is just as heavy as a pound of feathers?"
+    },
+q3: {
+    text: "how long is a foot?",
+    wa: ["6 centimeters", "10 toes", "18 pounds"],
+    ca: "12 inches",
+    additionalInfo: "did you know that 12 inches is also not that big"
+    }
+};
+
+
+function questionRandomizer () {
+    var qNum = Math.floor(Math.random() * qBank.length)
+}
+
+function newQuestion() {
+    //random number generator function to pick the new question from the bank
+$("#questionDisplay").text(qBank.q1.text)
+questionAnswers = $("<div>")
+    //random number generator to mix up the answers between rounds
+$("#a1").text(qBank.q1.wa[1])
+// DRY  i'll have to think about this later
+$("#a2").text(qBank.q1.wa[2])
+
+$("#a3").text(qBank.q1.wa[0])
+
+$("#a4").text(qBank.q1.ca)
+
+
+
+
+}
+newQuestion()
