43a8e43455af23c64a440fc3019e4a64f95cf810
pattenbranden <patten.branden@gmail.com>
2020-01-19 14:39:23 -0500

    changed html answerlist from span to div for event listener. added question randomizer and answer randomizer for replayability.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+
+
 // use custom font
 // greet player with instructions and start button on document ready 
 // hide instructions on game start 
@@ -7,54 +9,78 @@
 // do similar with wrong answer, show correct answer
 // after round is complete offer results and the option to resset the script 
 
-var qNum = 0
-var wrongAnswer
-var correctAnswer
+// var qNum = 2
+
+
+
+var wrongAnswer;
+var correctAnswer;
 
 
 var qBank = {
 
 q0: {
     text: "how long is a foot?",
-    wa: ["6 centimeters", "10 toes", "18 pounds"],
-    ca: "12 inches",
+    answers: ["12 inches", "6 centimeters", "10 toes", "18 pounds"],
+    correct: 0,
     additionalInfo: "did you know that 12 inches is also not that big"
     },
 
 q1: {
     text: "how heavy is a pound of feathers?",
-    wa: ["3 stone", "12 kilograms", "14 miles"],
-    ca: "a pound",
+    answers: ["a pound", "3 stone", "12 kilograms", "14 miles"],
+    correct: 0,
     additionalInfo: "did you know that a pound of steel is just as heavy as a pound of feathers?"
     },
-q3: {
+q2: {
     text: "how long is a foot?",
-    wa: ["6 centimeters", "10 toes", "18 pounds"],
-    ca: "12 inches",
+    answers: ["12 inches", "6 centimeters", "10 toes", "18 pounds"],
+    correct: 0,
     additionalInfo: "did you know that 12 inches is also not that big"
-    }
+    },
+    
+};
+
+$(document).ready(function() {
+    var qNum
+
+
+function newQuestion() {
+    qNum = ("q" + Math.floor(Math.random() * (qBank.q0.answers.length - 1)));
+    array = qBank[qNum].answers;
+    $("#questionDisplay").text(qBank[qNum].text);
+    
+    for (let i = 0; i < (qBank[qNum].answers.length +3); i++) {
+        j = Math.floor(Math.random() * (array.length ));
+        $(".answer").eq(i).text(array[j]);
+        array.splice(j, 1);
+        
+        document.getElementById("answerList").addEventListener("click",function(e) {if (e.target && e.target == (qBank[qNum].answers[0])) {
+        alert("NAiled it! " + qBank[qNum].additionalInfo);
+        // start timer 
+        newQuestion();
+        } else { 
+            alert("womp. :( sorry. ) " + qBank[qNum].additionalInfo);
+            newQuestion();
+        
+       };
+
+        
+        
+    });
+    
+
+};
+    
+
+
 };
 
 
-function questionRandomizer () {
-    var qNum = Math.floor(Math.random() * qBank.length)
-}
-
-function newQuestion() {
-    //random number generator function to pick the new question from the bank
-$("#questionDisplay").text(qBank.q1.text)
-questionAnswers = $("<div>")
-    //i want to add a random number generator to mix up the answers index between rounds
-$("#a1").text(qBank.q1.wa[1])
-// DRY  i'll have to think about this later, should be a for loop with array index++
-$("#a2").text(qBank.q1.wa[2])
-
-$("#a3").text(qBank.q1.wa[0])
-
-$("#a4").text(qBank.q1.ca)
 
 
 
 
-}
-newQuestion()
+newQuestion();
+
+});
